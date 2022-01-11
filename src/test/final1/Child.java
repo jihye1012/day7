e771d5712257dcb908e1e3defdc9015be8a4944b
yjh05 <yjh05@192.168.219.101>
2022-01-11 15:23:16 +0900

    Completion Parent

---
+++ src/test/final1/Child.java
@@ -0,0 +1,10 @@
+package test.final1;
+
+public class Child extends Parent{
+	public int time = 3;
+	
+	public void rest() {
+		System.out.println(time+"시간 휴식한다.");
+	}
+
+}
---
+++ src/test/final1/Parent.java
@@ -0,0 +1,8 @@
+package test.final1;
+
+public class Parent{
+	public void rest() {
+		System.out.println("10분간 휴식한다.");
+	}
+
+}
